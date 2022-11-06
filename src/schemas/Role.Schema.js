28035192c48a3c19dc33a877422f22a949875fbb
DianaLauraMN <dianaalauramn@gmail.com>
2022-11-06 16:51:55 -0700

    Role Schema was updated

--- src/schemas/Role.Schema.js
+++ src/schemas/Role.Schema.js
@@ -1,11 +1,6 @@
 import mongoose from "mongoose";
 
 const Role = new mongoose.Schema({
-    id: {
-        type: String,
-        required: true,
-        unique: true,
-    },
     type: {
         type: String,
         maxlength: 20,
