11744549a49c210a810e3889b2b6e37a892ba64a
DianaLauraMN <dianaalauramn@gmail.com>
2022-11-07 19:14:15 -0700

    SM-4 #comment Product Schema was created

---
+++ src/schemas/Product.Schema.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const Product = new mongoose.Schema({
+    name: {
+        type: String,
+        maxlength: 50,
+        required: true,
+    }, price: {
+        type: Number,
+        required: true,
+    }, stock: {
+        type: Number,
+        required: true,
+    }, description: {
+        type: String,
+        maxlength: 50,
+        required: true,
+    }, images: {
+        type: [String],
+    }, rate: {
+        type: Number,
+    }, category: {
+        type: Types.ObjectId,
+        ref: "Category",
+        required: true,
+    }
+}, { versionKey: false });
+
+export default mongoose.model("Product", Product);
